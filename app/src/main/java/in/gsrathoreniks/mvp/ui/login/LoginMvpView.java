f92c3b9ac8bf5aeffce31c279cc5a01cf6a2cd04
Gajendra singh Rathore <gajendrasinghrathore@Gajendras-MacBook-Pro.local>
2018-03-13 06:01:25 +0530

    LoginMvpView added

---
+++ app/src/main/java/in/gsrathoreniks/mvp/ui/login/LoginMvpView.java
@@ -0,0 +1,15 @@
+package in.gsrathoreniks.mvp.ui.login;
+
+import in.gsrathoreniks.mvp.ui.base.MvpView;
+
+/**
+ * Created by gajendrasinghrathore on 13/03/18.
+ */
+
+public interface LoginMvpView extends MvpView {
+
+    void openMainActivity();
+
+    void onLoginButtonClick();
+
+}
