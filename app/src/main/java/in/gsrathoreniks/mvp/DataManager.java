418b82544d725fe05dd8709fc4a29912fa3db344
Gajendra singh Rathore <gajendrasinghrathore@Gajendras-MacBook-Pro.local>
2018-03-13 05:41:17 +0530

    DataManager added

---
+++ app/src/main/java/in/gsrathoreniks/mvp/DataManager.java
@@ -0,0 +1,34 @@
+package in.gsrathoreniks.mvp;
+
+/**
+ * Created by gajendrasinghrathore on 13/03/18.
+ */
+
+public class DataManager {
+
+    SharedPrefsHelper mSharedPrefsHelper;
+
+    public DataManager(SharedPrefsHelper sharedPrefsHelper) {
+        mSharedPrefsHelper = sharedPrefsHelper;
+    }
+
+    public void clear() {
+        mSharedPrefsHelper.clear();
+    }
+
+    public void saveEmailId(String email) {
+        mSharedPrefsHelper.putEmail(email);
+    }
+
+    public String getEmailId() {
+        return mSharedPrefsHelper.getEmail();
+    }
+
+    public void setLoggedIn() {
+        mSharedPrefsHelper.setLoggedInMode(true);
+    }
+
+    public Boolean getLoggedInMode() {
+        return mSharedPrefsHelper.getLoggedInMode();
+    }
+}
