248b9d0e863ecb81f901c46bf7b2bb91652610ca
Gajendra singh Rathore <gajendrasinghrathore@Gajendras-MacBook-Pro.local>
2018-03-13 05:55:01 +0530

    SplashMvpView added

---
+++ app/src/main/java/in/gsrathoreniks/mvp/ui/splash/SplashMvpView.java
@@ -0,0 +1,15 @@
+package in.gsrathoreniks.mvp.ui.splash;
+
+import in.gsrathoreniks.mvp.ui.base.MvpView;
+
+/**
+ * Created by gajendrasinghrathore on 13/03/18.
+ */
+
+public interface SplashMvpView extends MvpView {
+
+    void openMainActivity();
+
+    void openLoginActivity();
+
+}
