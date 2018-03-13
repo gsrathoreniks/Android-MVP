3fd232fee907c376bdf597ea4079dce933b0ccd2
Gajendra singh Rathore <gajendrasinghrathore@Gajendras-MacBook-Pro.local>
2018-03-13 05:50:08 +0530

    Base Activity added

---
+++ app/src/main/java/in/gsrathoreniks/mvp/ui/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package in.gsrathoreniks.mvp.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gajendrasinghrathore on 13/03/18.
+ */
+
+public class BaseActivity extends AppCompatActivity implements MvpView {
+
+}
